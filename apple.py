4803b883ffa066ccdf40ee94bfcd548b02d89197
Roma <166724833+Romchik009@users.noreply.github.com>
2024-12-12 17:03:03 +0300

    Create apple.py

---
+++ apple.py
@@ -0,0 +1,11 @@
+from random import randrange
+
+
+class Apple():
+    def __init__(self, MAP_SIZE, **kwargs):
+        super().__init__(**kwargs)
+        self.MAP_SIZE = MAP_SIZE
+        self.new_position()
+
+    def new_position(self):
+        self.position = (randrange(self.MAP_SIZE) + 0.5, randrange(self.MAP_SIZE) + 0.5, -0.5)
