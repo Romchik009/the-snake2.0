9977fb78934320f7b7fa408a0235d75c7575c931
Roma <166724833+Romchik009@users.noreply.github.com>
2025-01-13 22:12:56 +0300

    Update gameobjects.py

--- gameobjects.py
+++ gameobjects.py
@@ -1,22 +1,20 @@
-from pygame import 
 from random import randrange
+from pygame import Entity, Vec3, color  # Импортируем необходимые модули из pygame
 
-
-class Apple(Entity):
+class Apple(Entity):  # Класс для создания яблок
     def __init__(self, MAP_SIZE, **kwargs):
         super().__init__(**kwargs)
         self.MAP_SIZE = MAP_SIZE
         self.new_position()
 
-    def new_position(self):
+    def new_position(self):  # Устанавливаем новую позицию яблока
         self.position = (randrange(self.MAP_SIZE) + 0.5, randrange(self.MAP_SIZE) + 0.5, -0.5)
 
 
-class Snake:
+class Snake:  # Класс для создания змеи
     def __init__(self, MAP_SIZE):
         self.MAP_SIZE = MAP_SIZE
         self.segment_length = 1
-        self.position_length = self.segment_length + 1
         self.segment_positions = [Vec3(randrange(MAP_SIZE) + 0.5, randrange(MAP_SIZE) + 0.5, -0.5)]
         self.segment_entities = []
         self.create_segment(self.segment_positions[0])
@@ -27,31 +25,31 @@ class Snake:
         self.speed, self.score = 12, 0
         self.frame_counter = 0
 
-    def add_segment(self):
+    def add_segment(self):  # Добавляем новый сегмент змеи
         self.segment_length += 1
-        self.position_length += 1
         self.score += 1
         self.speed = max(self.speed - 1, 5)
-        self.create_segment(self.segment_positions[0])
+        self.create_segment(self.segment_positions[-1])
 
-    def create_segment(self, position):
-        entity = Entity(position=position)
-        Entity(model='sphere', color=color.green, position=position).add_script(
-            SmoothFollow(speed=12, target=entity, offset=(0, 0, 0)))
-        self.segment_entities.insert(0, entity)
+    def create_segment(self, position):  # Создаем сегмент змеи
+        entity = Entity(model='sphere', color=color.green, position=position)
+        self.segment_entities.append(entity)
 
-    def run(self):
+    def run(self):  # Обновляем позиции сегментов змеи
         self.frame_counter += 1
         if not self.frame_counter % self.speed:
-            self.segment_positions.append(self.segment_positions[-1] + self.direction)
-            self.segment_positions = self.segment_positions[-self.segment_length:]
-            for segment, segment_position in zip(self.segment_entities, self.segment_positions):
-                segment.position = segment_position
+            new_position = self.segment_positions[-1] + self.direction
+            # Проверяем, не вышла ли змея за пределы карты
+            if 0 <= new_position.x < self.MAP_SIZE and 0 <= new_position.y < self.MAP_SIZE:
+                self.segment_positions.append(new_position)
+                self.segment_positions = self.segment_positions[-self.segment_length:]
+                for segment, segment_position in zip(self.segment_entities, self.segment_positions):
+                    segment.position = segment_position
 
-    def control(self, key):
-        for pressed_key in 'wasd':
-            if pressed_key == key and self.permissions[pressed_key]:
-                self.direction = self.directions[pressed_key]
+    def control(self, key):  # Управление змеей
+        if key in self.directions and self.permissions[key]:
+            # Запрещаем движение в противоположном направлении
+            if self.direction != -self.directions[self.taboo_movement[key]]:
+                self.direction = self.directions[key]
                 self.permissions = dict.fromkeys(self.permissions, 1)
-                self.permissions[self.taboo_movement[pressed_key]] = 0
-                break
+                self.permissions[self.taboo_movement[key]] = 0
